c1a54779023ae06a00e8e92a33c0e225a49e588f
yujin won <won_yujin@naver.com>
2022-03-21 13:42:34 +0900

    feat: 🎸 [FE] add navbar and install styling libraries, router

---
+++ frontend/src/routes/MyInfo.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function MyInfo() {
+  return (
+    <>
+      <Navbar />
+      <h1>MyInfo</h1>
+    </>
+  );
+}
+
+export default MyInfo;
---
+++ frontend/src/routes/Recommendation.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function Recommendation() {
+  return (
+    <>
+      <Navbar />
+      <h1>Recommendation</h1>
+    </>
+  );
+}
+
+export default Recommendation;
