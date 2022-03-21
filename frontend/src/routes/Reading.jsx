92959296dc7cdbc0ad9df36f0f8133afdc3c00c8
woori9 <wrh95222@gmail.com>
2022-03-22 00:03:41 +0900

    feat: 🎸 [front] 책 읽는 중 페이지에 스톱 워치 기능 추가
    책 읽는 중 페이지에 들어가는 순간 카운트 시작, 일시정지/재생 버튼, 2분이 지나면 타이머 사라짐

--- frontend/src/routes/Reading.jsx
+++ frontend/src/routes/Reading.jsx
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import formatTime from '../utils/formatTime';
+
 function Reading() {
+  const [isActive, setIsActive] = useState(true);
+  const [isTimeVisible, setIsTimeVisible] = useState(true);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let ticking = null;
+
+    if (isActive) {
+      ticking = setInterval(() => {
+        setTime(prevTime => {
+          if (prevTime === 120) {
+            setIsTimeVisible(false);
+          }
+          return prevTime + 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(ticking);
+    }
+    return () => {
+      clearInterval(ticking);
+    };
+  }, [isActive]);
+
   return (
     <div>
       <h1>Reading</h1>
+      <h3>Stopwatch</h3>
+      {isTimeVisible && <h1>{formatTime(time)}</h1>}
+      <h1>{isActive ? '독서 중' : '쉬는 중'}</h1>
+      <button type="button" onClick={() => setIsActive(!isActive)}>
+        {isActive ? '일시정지' : '재생'}
+      </button>
     </div>
   );
 }
