148ff062fe7e830909d0ddfdf912e93064d9af74
shl128 <shl128@naver.com>
2022-03-23 12:51:55 +0900

    feat: 🎸 [back] 서재 api 완성

--- backend/kkubooks/urls.py
+++ backend/kkubooks/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('main/book/<int:book_id>/', main.book_detail),
     path('main/<int:book_id>/commit/', main.commit),
     path('main/commits/', main.commit_list),
-    path('main/<int:book_id>/rating/', main.rating)
+    path('main/<int:book_id>/rating/', main.rating),
 
     path('bookshelf/', bookshelf.create_book),
     path('bookshelf/<int:book_id>/', bookshelf.bookshelf_detail),
-    path('bookshelf/booklist/', bookshelf.bookshelf_list)
-    path('bookshelf/<int:book_id>/memolist', bookshelf.get_memo),
+    path('bookshelf/booklist/', bookshelf.bookshelf_list),
+    path('bookshelf/<int:book_id>/memolist', bookshelf.get_memo)
 ]
