6e946260a2467536c5798b741c0b3d8aebf85e26
최시열 <continue75@naver.com>
2022-03-23 18:06:21 +0900

    refactor: 💡 [back] 공백 문자열 수정

--- backend/kkubooks/serializers/commit.py
+++ backend/kkubooks/serializers/commit.py
@@ -3,7 +3,7 @@ from ..models import Commit
 
 class CommitSerializer(serializers.ModelSerializer):
 
-  class Meta:
-    model = Commit
-    fields = '__all__'
-    read_only_fields = ('user_id', 'book_id', )
+	class Meta:
+		model = Commit
+		fields = '__all__'
+		read_only_fields = ('user_id', 'book_id', )
