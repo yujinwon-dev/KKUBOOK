a9b4686ca76a150166a044f07ba284b58e29d9f6
woori9 <wrh95222@gmail.com>
2022-03-23 11:10:21 +0900

    refactor: 💡 [front] Reading 컴포넌트에서 시간을 보여주는 부분을 Time 컴포넌트로 분리
    Reading 컴포넌트 성능을 높이기 위해 시간을 보여주는 부분을 분리시킴

--- frontend/src/routes/Reading.jsx
+++ frontend/src/routes/Reading.jsx
@@ -1,41 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import formatTime from '../utils/formatTime';
+import React, { useState } from 'react';
+import Time from '../components/main/Time';
 
 function Reading() {
   const [isActive, setIsActive] = useState(true);
   const [isTimeVisible, setIsTimeVisible] = useState(true);
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    let ticking = null;
-
-    if (isActive) {
-      ticking = setInterval(() => {
-        setTime(prevTime => {
-          if (prevTime === 120) {
-            setIsTimeVisible(false);
-          }
-          return prevTime + 1;
-        });
-      }, 1000);
-    } else {
-      clearInterval(ticking);
-    }
-    return () => {
-      clearInterval(ticking);
-    };
-  }, [isActive]);
 
   return (
-    <div>
+    <>
       <h1>Reading</h1>
       <h3>Stopwatch</h3>
-      {isTimeVisible && <h1>{formatTime(time)}</h1>}
+      {isTimeVisible && (
+        <Time isActive={isActive} setIsTimeVisible={setIsTimeVisible} />
+      )}
       <h1>{isActive ? '독서 중' : '쉬는 중'}</h1>
       <button type="button" onClick={() => setIsActive(!isActive)}>
         {isActive ? '일시정지' : '재생'}
       </button>
-    </div>
+    </>
   );
 }
 
