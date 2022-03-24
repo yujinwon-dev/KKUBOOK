0c1cb21d09c41d62872ac6105b66ca3283f39a73
yujin won <won_yujin@naver.com>
2022-03-24 15:02:56 +0900

    feat: 🎸 [front] 로그인 후 유저 데이터 저장

--- frontend/src/routes/KakaoRedirectHandler.jsx
+++ frontend/src/routes/KakaoRedirectHandler.jsx
@@ -1,10 +1,13 @@
+import axios from 'axios';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getToken, getUserInfo } from '../api/user';
+import useStore from '../stores/user';
 
 function KakaoRedirectHandler() {
   // 인가코드
   const code = new URL(window.location.href).searchParams.get('code');
+  const setUserInfo = useStore(state => state.setUserInfo);
   const navigate = useNavigate();
 
   React.useEffect(() => {
@@ -16,11 +19,23 @@ function KakaoRedirectHandler() {
         code,
       },
       response => {
-        console.log('login');
         const accessToken = response.data.access_token;
         getUserInfo(accessToken, res => {
+          const {
+            user_id,
+            nickname,
+            kkubook_complete,
+            is_kkubook,
+            access_token,
+          } = res.data;
+          axios.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+          setUserInfo({
+            userId: user_id,
+            kkubookComplete: kkubook_complete,
+            isKkubook: is_kkubook,
+            nickname,
+          });
           navigate('/');
-          console.log(res);
         });
       },
       () => {
--- frontend/src/routes/MyInfo.jsx
+++ frontend/src/routes/MyInfo.jsx
@@ -1,12 +1,16 @@
 import Navbar from '../components/common/Navbar';
 import FabButton from '../components/common/FabButton';
+import useStore from '../stores/user';
 
 function MyInfo() {
+  const userInfo = useStore(state => state.userInfo);
+  const { kkubookComplete, isKkubook, nickname } = userInfo;
   return (
     <>
       <Navbar />
       <FabButton />
       <h1>MyInfo</h1>
+      <p>{nickname}님</p>
     </>
   );
 }
