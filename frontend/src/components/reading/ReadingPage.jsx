944672cb8e4bee74e77ad55f31daae5f8d883db7
woori9 <wrh95222@gmail.com>
2022-03-24 20:51:14 +0900

    refactor: 💡 [front] 책 정보를 보여주는 부분을 Book 컴포넌트로 분리
    시간이 바뀌면서 책 정보가 불필요하게 렌더링되는 것을 방지하기 위해 Book컴포넌트로 분리 및 memo hook 사용

--- frontend/src/components/reading/ReadingPage.jsx
+++ frontend/src/components/reading/ReadingPage.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Time from './Time';
+import Book from './Book';
 
 const StyledReadingPage = styled.div`
   background-color: #2a4753;
@@ -29,30 +30,6 @@ const StyledReadingPage = styled.div`
   }
 `;
 
-const BookReading = styled.div`
-  background-color: white;
-  margin: 1rem auto;
-  height: 15vh;
-  width: 90%;
-  border-radius: 10px;
-  background-color: rgba(255, 255, 255, 0.5);
-  display: flex;
-  align-items: center;
-
-  img {
-    height: 120px;
-    width: 80px;
-    margin: 10px;
-  }
-
-  button {
-    margin-left: auto;
-    height: 100%;
-    border-radius: 0px 10px 10px 0px;
-    border: 0;
-  }
-`;
-
 function ReadingPage({
   time,
   isTimerActive,
@@ -74,16 +51,11 @@ function ReadingPage({
         {isTimeVisible && <Time time={time} />}
       </div>
 
-      <BookReading>
-        <img src={book.image} alt={book.title} />
-        <div>
-          <h3>{book.title}</h3>
-          <p>{book.author}</p>
-        </div>
-        <button type="button" onClick={() => setIsTimerActive(!isTimerActive)}>
-          {isTimerActive ? '일시정지' : '재생'}
-        </button>
-      </BookReading>
+      <Book
+        book={book}
+        isTimerActive={isTimerActive}
+        setIsTimerActive={setIsTimerActive}
+      />
 
       <div className="button-container">
         <button type="button">메모하기</button>
