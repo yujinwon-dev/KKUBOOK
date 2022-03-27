a45933070e2bd651b27fdd3ad98e68c70b18ea4a
woori9 <wrh95222@gmail.com>
2022-03-28 08:20:19 +0900

    feat: 🎸 [front] 서재에서 읽은 책, 읽고 있는 책 등 카테고리 별로 선택할 수 있도록 구현
    선택된 카테고리에 따라 다른 책 목록을 렌더링하기 위해 서재 내부에 카테고리를 nav로 구현

--- frontend/src/routes/Bookshelf.jsx
+++ frontend/src/routes/Bookshelf.jsx
@@ -1,10 +1,62 @@
+import React, { useState } from 'react';
+import { styled } from 'twin.macro';
 import Navbar from '../components/common/Navbar';
 import FabButton from '../components/common/FabButton';
+import bookshelfCategories from '../constants/bookShelf';
+import BookshelfCategory from '../components/bookshelf/BookshelfCategory';
+
+const BookshelfPage = styled.div`
+  header {
+    padding-top: 50px;
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  .title {
+    margin-left: 10px;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    margin: 50px 10px 0px;
+    padding: 5px;
+  }
+
+  li {
+    list-style-type: none;
+  }
+`;
 
 function BookShelf() {
+  const [selectedCategory, setSelectedCategory] = useState(
+    bookshelfCategories[0],
+  );
+
+  const selectCategory = category => {
+    if (category.name !== selectedCategory.name) {
+      setSelectedCategory(category);
+    }
+  };
+
   return (
     <>
-      BookShelf
+      <BookshelfPage>
+        <header>
+          <p className="title">내 서재</p>
+        </header>
+
+        <ul>
+          {bookshelfCategories.map(category => (
+            <BookshelfCategory
+              key={category.status}
+              category={category}
+              isSelected={selectedCategory.name === category.name}
+              handleClick={selectCategory}
+            />
+          ))}
+        </ul>
+      </BookshelfPage>
       <Navbar />
       <FabButton />
     </>
