75d229936c3eb0635ceabd397a6bb00980c9a0f7
woori9 <wrh95222@gmail.com>
2022-03-28 08:15:13 +0900

    refactor: 💡 [front] library 네이밍 bookshelf로 변경
    api 및 DB와 네이밍을 통일하기 위해 bookshelf로 변경함

--- frontend/src/routes/Bookshelf.jsx
+++ frontend/src/routes/Bookshelf.jsx
@@ -1,14 +1,14 @@
 import Navbar from '../components/common/Navbar';
 import FabButton from '../components/common/FabButton';
 
-function Library() {
+function BookShelf() {
   return (
     <>
+      BookShelf
       <Navbar />
       <FabButton />
-      <h1>Library</h1>
     </>
   );
 }
 
-export default Library;
+export default BookShelf;
