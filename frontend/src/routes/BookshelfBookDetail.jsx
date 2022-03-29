672a34f07b9250682695c31adcaec0e6fdde0615
woori9 <wrh95222@gmail.com>
2022-03-30 01:17:51 +0900

    feat: 🎸 [front] 서재의 책 디테일 페이지에서 책 상태에 따라 보이는 버튼 분기 처리
    서재의 책의 상태에 따라 책 디테일에서 header의 삭제 버튼 유무, footer의 읽기 버튼 상태 분기 처리

--- frontend/src/routes/BookshelfBookDetail.jsx
+++ frontend/src/routes/BookshelfBookDetail.jsx
@@ -4,6 +4,7 @@ import useBookStore from '../stores/book';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import BookDetail from '../components/bookshelf/BookDetail';
+import Button from '../components/common/Button';
 
 function BookshelfBook() {
   const navigate = useNavigate();
@@ -25,15 +26,56 @@ function BookshelfBook() {
     }
   }, []);
 
+  const moveToReading = () => {
+    navigate(`/reading/${bookId}`);
+  };
+
+  const getButtonByStatus = {
+    1: <Button title="읽기" width="90%" onClick={moveToReading} />,
+    2: (
+      <Button
+        title="읽기 시작"
+        width="90%"
+        onClick={() => {
+          // update book status
+          moveToReading();
+        }}
+      />
+    ),
+    3: (
+      <Button
+        title="읽기"
+        width="90%"
+        onClick={() => {
+          // update book status
+          moveToReading();
+        }}
+      />
+    ),
+  };
+
+  const getHeaderButton = status => {
+    if (status === 2 || status === 3) {
+      return (
+        <Button
+          title="삭제"
+          background="transparent"
+          color="gray"
+          width="initial"
+          size="15px"
+          padding="14px"
+        />
+      );
+    }
+
+    return null;
+  };
+
   return (
     <>
-      <Header />
+      <Header>{getHeaderButton(book.status)}</Header>
       {book && <BookDetail book={book} />}
-      {book.status !== 0 && (
-        <Footer>
-          <button type="button">책 읽기</button>
-        </Footer>
-      )}
+      {book.status !== 0 && <Footer>{getButtonByStatus[book.status]}</Footer>}
     </>
   );
 }
