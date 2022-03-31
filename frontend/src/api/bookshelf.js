26ec97874d6c21630deca5a018eea7eaf830b41b
woori <wrh95222@gmail.com>
2022-03-31 23:10:06 +0900

    feat: 🎸 [front] BookDetail컴포넌트에서 책 추가 api 연결 및 책 추가 후 서재로 이동

--- frontend/src/api/bookshelf.js
+++ frontend/src/api/bookshelf.js
@@ -6,8 +6,20 @@ const getBooks = async () => {
     const { data } = await api.get('kkubooks/bookshelf/booklist');
     return data.map(bookObj => formatKey(bookObj));
   } catch (err) {
-    return console.err(err);
+    return console.error(err);
   }
 };
 
-export default getBooks;
+const addBook = async (book, user) => {
+  try {
+    const { data } = await api.post('kkubooks/bookshelf/', {
+      book,
+      user,
+    });
+    return formatKey(data);
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
+export { getBooks, addBook };
--- frontend/src/stores/bookshelf.js
+++ frontend/src/stores/bookshelf.js
@@ -1,6 +1,7 @@
 import create from 'zustand';
 import bookshelfCategories from '../constants/bookShelf';
-import getBooks from '../api/bookshelf';
+import { getBooks } from '../api/bookshelf';
+import books from '../data/books';
 
 const useStore = create((set, get) => ({
   books: [],
@@ -9,7 +10,8 @@ const useStore = create((set, get) => ({
     return set({ selectedCategory: category });
   },
   getBooklist: async () => {
-    const books = await getBooks();
+    // 현재 필요한 book정보가 전부 담겨있지 않아 api 요청대신  mock data를 사용합니다.
+    // const books = await getBooks();
     return set({ books });
   },
 }));
