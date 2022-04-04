dab980d3a601dad3560f0806a6e4d6ae8c8f0edb
yujin won <won_yujin@naver.com>
2022-04-04 16:04:54 +0900

    refactor: 💡 [front] 빈 화면 문구 수정 및 꾸북 이미지 추가

--- frontend/src/components/myinfo/EmptyMessage.jsx
+++ frontend/src/components/myinfo/EmptyMessage.jsx
@@ -3,7 +3,14 @@ import backKkubook from '../../assets/back-kkubook.png';
 
 const MessageDiv = styled.div`
   ${tw`flex flex-col justify-center items-center`}
-  height: 250px;
+
+  @media screen and (max-width: 375px) {
+    height: 150px;
+  }
+
+  @media screen and (min-width: 376px) {
+    height: 250px;
+  }
 
   img {
     display: block;
@@ -17,7 +24,7 @@ function EmptyMessage() {
   return (
     <MessageDiv>
       <img src={backKkubook} alt="back of kkubook character" />
-      <p>이번 달에 읽은 책이 없어요.</p>
+      <p>이번 달에 읽은 책이 없습니다.</p>
     </MessageDiv>
   );
 }
