ef4c6408e684ff9c4ab83e3af1b3359420d2ae7a
syc1008 <syc1008@naver.com>
2022-04-05 11:47:25 +0900

    refactor: 💡 [front] 메모 - img 오류 수정 -seungyeon

--- frontend/src/utils/memoFormat.js
+++ frontend/src/utils/memoFormat.js
@@ -4,6 +4,7 @@ const memoFormat = obj => {
     content: obj.content,
     created_at: obj.created_at,
     id: obj.id,
+    is_img: obj.memo_img,
     memo_img: `http://127.0.0.1:8000${obj.memo_img}`,
     memo_mark: obj.memo_mark,
     updated_at: obj.updated_at,
