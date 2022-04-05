321309d308749428844fd40d2d20f14ae03461f6
yujin won <won_yujin@naver.com>
2022-04-05 18:11:35 +0900

    refactor: 💡 [front] api base url 배포 서버 주소로 수정

--- frontend/src/utils/memoFormat.js
+++ frontend/src/utils/memoFormat.js
@@ -1,10 +1,12 @@
+import { API_BASE_URL } from './apiInstance';
+
 const memoFormat = obj => {
   const styledMemo = {
     book_info: obj.book_info,
     content: obj.content,
     created_at: obj.created_at,
     id: obj.id,
-    memo_img: `http://127.0.0.1:8000${obj.memo_img}`,
+    memo_img: `${API_BASE_URL}${obj.memo_img}`,
     memo_mark: obj.memo_mark,
     updated_at: obj.updated_at,
   };
