643d255293620ddd5f39271aaa3f8fc97ec35f0b
woori <wrh95222@gmail.com>
2022-04-05 11:25:05 +0900

    style: 💄 [front] Footer위쪽에 그라데이션 백그라운드 추가

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -7,11 +7,30 @@ const StyledFooter = styled.footer`
   background-color: white;
   bottom: 0px;
   text-align: center;
-  padding: 15px 0px;
+  padding: 0px 0px 15px;
+`;
+
+const GradientBox = styled.div`
+  position: absolute;
+  top: -24px;
+  left: 0px;
+  width: 100%;
+  height: 24px;
+  z-index: 2;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 0.95) 99.9%,
+    rgba(255, 255, 255, 0.95) 99.99%
+  );
 `;
 
 function Footer({ children }) {
-  return <StyledFooter>{children}</StyledFooter>;
+  return (
+    <StyledFooter>
+      <GradientBox />
+      {children}
+    </StyledFooter>
+  );
 }
 
 export default Footer;
